6a3ab69c3eee22139820fbddc41d31d2b7ad917f
caraka wijaya <109886433+caraka20@users.noreply.github.com>
2024-12-07 13:28:35 +0700

    done

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    headers: {
-        'apikey': import.meta.env.VITE_API_KEY
-    }
+  baseURL: "http://192.168.1.155:3000",
+  headers: {
+    apikey: "tekno123",
+  },
 });
 
 export default axiosInstance;
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//     baseURL: import.meta.env.VITE_API_BASE_URL,
+//     headers: {
+//         'apikey': import.meta.env.VITE_API_KEY
+//     }
+// });
+
+// export default axiosInstance;
